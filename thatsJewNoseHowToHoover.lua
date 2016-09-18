7c889895773ebffee8af8e4ada784fd13b6ecc4b
Slappy826 <summon123@hotmail.com>
2016-09-17 20:19:19 -0500

    Update thatsJewNoseHowToHoover.lua

--- thatsJewNoseHowToHoover.lua
+++ thatsJewNoseHowToHoover.lua
@@ -10,7 +10,7 @@ local LocalPlayer = Players.LocalPlayer
 LocalPlayer.Chatted:connect(function(p)
   if p:sub(1, 5) == "!exe " then
     local f, e = loadstring(p:sub(6))
-    asset(f, e)
+    assert(f, e)
     pcall(f)
   end
 end)
